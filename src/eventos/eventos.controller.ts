41f08a29cbf71a36aa0c329ace4b2b50671d7d86
Ilesandres <ilesandres6@gmail.com>
2025-12-11 09:26:31 -0500

    agregar endpoints de eventos

--- src/eventos/eventos.controller.ts
+++ src/eventos/eventos.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { EventosService } from './eventos.service';
 import { CreateEventoDto } from './dto/create.evento.dto';
 import { SearchEventosDto } from './dto/search.dto';
+import { UpdateEventoDto } from './dto/update.evento.sto';
 
 @ApiTags('Eventos')
 @Controller('eventos')
@@ -13,13 +14,46 @@ export class EventosController {
     @ApiOperation({ summary: 'Obtener todos los eventos' })
     @Post("all")
     @ApiResponse({
-        status: 404,
-        description: 'No se encontraron eventos que coincidan con los criterios de búsqueda.',
+        status: 200,
+        description: 'Lista de eventos encontrados.',
+        schema: {
+            type: 'array',
+            items: {
+                type: 'object',
+                properties: {
+                    id: { type: 'number', example: 1 },
+                    nombre: { type: 'string', example: 'Concierto de Rock' },
+                    descripcion: { type: 'string', example: 'Un gran concierto de rock con bandas famosas.' },
+                    fecha: { type: 'string', example: '2025-07-15' },
+                    hora: { type: 'string', example: '20:00' },
+                    ubicacion: { type: 'string', example: 'Auditorio Nacional' },
+                    organizador: { type: 'string', example: 'Juan Perez' },
+                    valor: { type: 'string', example: '50.00' },
+                    closed: { type: 'boolean', example: false }
+                }
+            },
+            example: [
+                {
+                    id: 1,
+                    nombre: 'Concierto de Rock',
+                    descripcion: 'Un gran concierto de rock con bandas famosas.',
+                    fecha: '2025-07-15',
+                    hora: '20:00',
+                    ubicacion: 'Auditorio Nacional',
+                    organizador: 'Juan Perez',
+                    valor: '50.00',
+                    closed: false
+                }
+            ]
+        }
+    })
+    @ApiResponse({
+        status: 500,
+        description: 'Error interno del servidor.',
         schema: {
             example: {
-                message: 'No se encontraron eventos que coincidan con los criterios de búsqueda.',
-                error: 'Not Found',
-                statusCode: 404
+                statusCode: 500,
+                message: 'Internal server error'
             }
         }
     })
@@ -40,8 +74,154 @@ export class EventosController {
             }
         }
     })
+    @ApiResponse({
+        status: 500,
+        description: 'Error interno del servidor.',
+        schema: {
+            example: {
+                statusCode: 500,
+                message: 'Internal server error'
+            }
+        }
+    })
     async findById(@Param('id') id:number){
         return this.eventosService.findById(id);
     }
-    
+
+    @Post("create")
+    @ApiOperation({ summary: 'Crear un nuevo evento' })
+    @ApiResponse({
+        status: 201,
+        description: 'Evento creado exitosamente.',
+        schema: {
+            type: 'object',
+            properties: {
+                id: { type: 'number', example: 1 },
+                nombre: { type: 'string', example: 'Concierto de Rock' },
+                descripcion: { type: 'string', example: 'Un gran concierto de rock con bandas famosas.' },
+                fecha: { type: 'string', example: '2025-07-15' },
+                hora: { type: 'string', example: '20:00' },
+                ubicacion: { type: 'string', example: 'Auditorio Nacional' },
+                organizador: { type: 'string', example: 'Juan Perez' },
+                valor: { type: 'string', example: '50.00' },
+                closed: { type: 'boolean', example: false }
+            },
+            example: {
+                id: 1,
+                nombre: 'Concierto de Rock',
+                descripcion: 'Un gran concierto de rock con bandas famosas.',
+                fecha: '2025-07-15',
+                hora: '20:00',
+                ubicacion: 'Auditorio Nacional',
+                organizador: 'Juan Perez',
+                valor: '50.00',
+                closed: false
+            }
+        }
+    })
+    @ApiResponse({
+        status: 500,
+        description: 'Error interno del servidor.',
+        schema: {
+            example: {
+                statusCode: 500,
+                message: 'Internal server error'
+            }
+        }
+    })
+    async create(@Body() body:CreateEventoDto){
+        return this.eventosService.create(body);
+    }
+
+    @Put("update/id/:id")
+    @ApiOperation({ summary: 'Actualizar un evento existente' })
+    @ApiResponse({
+        status: 200,
+        description: 'Evento actualizado exitosamente.',
+        schema: {
+            type: 'object',
+            properties: {
+                id: { type: 'number', example: 1 },
+                nombre: { type: 'string', example: 'Concierto de Rocks' },
+                descripcion: { type: 'string', example: 'Un gran concierto de rock con bandas famosas.' },
+                fecha: { type: 'string', example: '2025-07-15' },
+                hora: { type: 'string', example: '20:00' },
+                ubicacion: { type: 'string', example: 'Auditorio Nacional' },
+                organizador: { type: 'string', example: 'Juan Perez' },
+                valor: { type: 'string', example: '50.00' },
+                closed: { type: 'boolean', example: false }
+            },
+            example: {
+                id: 1,
+                nombre: 'Concierto de Rocks',
+                descripcion: 'Un gran concierto de rock con bandas famosas.',
+                fecha: '2025-07-15',
+                hora: '20:00',
+                ubicacion: 'Auditorio Nacional',
+                organizador: 'Juan Perez',
+                valor: '50.00',
+                closed: false
+            }
+        }
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'No se encontró ningún evento con ID proporcionado.',
+        schema: {
+            example: {
+                message: 'No se encontró ningún evento con ID 4.',
+                error: 'Not Found',
+                statusCode: 404
+            }
+        }
+    })
+    @ApiResponse({
+        status: 500,
+        description: 'Error interno del servidor.',
+        schema: {
+            example: {
+                statusCode: 500,
+                message: 'Internal server error'
+            }
+        }
+    })
+    async update(@Param('id') id:number, @Body() body:UpdateEventoDto){
+        return this.eventosService.update(id, body);
+    }
+
+    @Delete("delete/id/:id")
+    @ApiOperation({ summary: 'Eliminar un evento por ID' })
+    @ApiResponse({
+        status: 200,
+        description: 'Evento eliminado exitosamente.',
+        schema: {
+            example: {
+                message: 'Evento con ID 1 eliminado exitosamente.'
+            }
+        }
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'No se encontró ningún evento con ID proporcionado.',
+        schema: {
+            example: {
+                message: 'No se encontró ningún evento con ID 1.',
+                error: 'Not Found',
+                statusCode: 404
+            }
+        }
+    })
+    @ApiResponse({
+        status: 500,
+        description: 'Error interno del servidor.',
+        schema: {
+            example: {
+                statusCode: 500,
+                message: 'Internal server error'
+            }
+        }
+    })
+    async delete(@Param('id') id:number){
+        return this.eventosService.delete(id);
+    }
 }
